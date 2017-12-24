f74431ba87ae569653e07e4d32a62a93dd1cc747
mark <markjmitry@gmail.com>
2017-12-23 21:25:14 -0800

    Add files via upload

---
+++ part3-processing_video.py
@@ -0,0 +1,36 @@
+import cv2
+from darkflow.net.build import TFNet
+import numpy as np
+import time
+
+option = {
+    'model': 'cfg/yolo.cfg',
+    'load': 'bin/yolo.weights',
+    'threshold': 0.15,
+    'gpu': 1.0
+}
+
+tfnet = TFNet(option)
+
+capture = cv2.VideoCapture('videofile_1080_20fps.avi')
+colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
+
+while (capture.isOpened()):
+    stime = time.time()
+    ret, frame = capture.read()
+    results = tfnet.return_predict(frame)
+    if ret:
+        for color, result in zip(colors, results):
+            tl = (result['topleft']['x'], result['topleft']['y'])
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            frame = cv2.rectangle(frame, tl, br, color, 7)
+            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        capture.release()
+        cv2.destroyAllWindows()
+        break
