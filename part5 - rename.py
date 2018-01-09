879bc9c69941058427a7d74141281462a2c17ec1
mark <markjmitry@gmail.com>
2018-01-08 21:35:09 -0800

    Add files via upload

---
+++ part5 - rename.py
@@ -0,0 +1,13 @@
+import os
+
+imdir = 'images'
+if not os.path.isdir(imdir):
+    os.mkdir(imdir)
+
+fidget_folders = [folder for folder in os.listdir('.') if 'fidget' in folder]
+
+n = 0
+for folder in fidget_folders:
+    for imfile in os.scandir(folder):
+        os.rename(imfile.path, os.path.join(imdir, '{:06}.png'.format(n)))
+        n += 1
